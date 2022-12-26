218cdf9555bfb149f59ac419964b9f93bb6c0658
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2022-12-26 12:03:32 +0530

    Register crontroller and Express Validation added.

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,5 +1,21 @@
 import { Request,Response } from "express";
+import { Joi } from "express-validation";
+
+const registerValidation = Joi.object({
+    first_name:Joi.string().required(),
+    last_name:Joi.string(),
+    email:Joi.string().email().required(),
+    password:Joi.string().required(),
+    password_confirm:Joi.string().required(),
+});
 
 export function register(req:Request, res:Response){
-
+    const {error} = registerValidation.validate(req.body);
+    if(error){
+        return res.status(400).send(error.message);
+    }
+    if(req.body.password !== req.body.password_confirm){
+        return res.status(400).send("passwords don't match");
+    }
+    return res.send(req.body);
 }
