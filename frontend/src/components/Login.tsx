42f78d5e8075b427c9556be2390f7f78c27c0b86
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2023-01-01 15:57:39 +0530

    Login page functional

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, {SyntheticEvent, useState} from 'react';
+import axios from 'axios';
+import {Redirect} from 'react-router-dom';
 
 function Login() {
-  return (
-    <div className="ui container" style={{width: '50%'}}>
-      <h1>Login</h1>
-      <form className="ui form">
-        <div className="field">
-          <label>Email</label>
-          <input type="text" name="email" placeholder="Email ID" />
-        </div>
-        <div className="field">
-          <label>Password</label>
-          <input type="text" name="password" placeholder="Password" />
-        </div>
-        <button className="ui button" type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  const [responseMessage, setResponseMessage] = useState(null);
+
+  async function onSubmitHandler(e: SyntheticEvent) {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/login', {
+        email,
+        password,
+      });
+      if (response.status >= 400) {
+        setResponseMessage(response.data);
+      } else setResponseMessage(null);
+
+      // Redirect, when sussessfully logged in:
+      setRedirect(true);
+
+      console.log(response.data);
+    } catch (e: any) {
+      console.log(e.response.data);
+      setResponseMessage(e.response.data);
+    }
+  }
+
+  if (redirect) {
+    return <Redirect to="/" />;
+  }
+
+  function renderForm() {
+    return (
+      <div className="ui container" style={{width: '50%'}}>
+        <h1>Login</h1>
+        <form className="ui form" onSubmit={onSubmitHandler}>
+          <div className="field">
+            <label>Email</label>
+            <input type="text" name="email" placeholder="Email ID" onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className="field">
+            <label>Password</label>
+            <input type="text" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button className="ui button" type="submit">
+            Submit
+          </button>
+        </form>
+        {responseMessage && (
+          <div className="ui warning message">
+            <i className="warning icon"></i>
+            {responseMessage}
+          </div>
+        )}
+      </div>
+    );
+  }
+  return renderForm();
 }
 
 export default Login;
