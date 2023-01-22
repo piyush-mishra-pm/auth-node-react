b8947336104b3b342008c55c4cddad3e79f3d988
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2023-01-22 16:35:23 +0530

    404 page and Switch in React-Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import apiWrapper from './apis/apiWrapper';
 
 import RegisterForm from './pages/RegisterForm';
@@ -8,9 +8,10 @@ import Login from './pages/Login';
 import HeaderNav from './pages/HeaderNav';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
+import ToastContainerWrapper from './components/ToastContainerWrapper';
+import NotFound from './pages/NotFound';
 
 import './App.css';
-import ToastContainerWrapper from './components/ToastContainerWrapper';
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState(null);
@@ -31,15 +32,18 @@ function App() {
     <div>
       <BrowserRouter>
         <HeaderNav loggedInUser={loggedInUser} setLoggedInStatus={() => setLoggedInStatus(false)} />
-        <Route path="/register" component={RegisterForm} />
-        <Route path="/forgot" component={ForgotPassword} />
-        <Route path="/reset/:token" component={ResetPassword} />
-        <Route
-          path="/login"
-          exact
-          component={() => <Login loggedInUser={loggedInUser} setLoggedInStatus={() => setLoggedInStatus(true)} />}
-        />
-        <Route path="/" exact component={() => <Home loggedInUser={loggedInUser} />} />
+        <Switch>
+          <Route path="/register" exact component={RegisterForm} />
+          <Route path="/forgot" exact component={ForgotPassword} />
+          <Route path="/reset/:token" exact component={ResetPassword} />
+          <Route
+            path="/login"
+            exact
+            component={() => <Login loggedInUser={loggedInUser} setLoggedInStatus={() => setLoggedInStatus(true)} />}
+          />
+          <Route path="/" exact component={() => <Home loggedInUser={loggedInUser} />} />
+          <Route path="*" component={NotFound} />
+        </Switch>
         <ToastContainerWrapper />
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <h2 className="ui center aligned icon header">
+        <i className="circular bug icon"></i>
+        Page Not found!
+      </h2>
+    </div>
+  );
+}
+
+export default NotFound;
