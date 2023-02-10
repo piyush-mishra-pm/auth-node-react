b435ec99f61f45bc70dd6096e0db892d350f99c1
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2023-02-10 16:21:59 +0530

    cors code added.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,6 +22,13 @@ const app = express();
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  next()
+});
+
 app.use(cors({ origin: KEYS.FE_ORIGIN, credentials: true }));
 
 app.use(passport.initialize());
